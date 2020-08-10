ee16ad31aefd8935d90815b720385c53e1441280
Prinjumon <prinjumon@outlook.com>
2020-08-10 09:18:20 +0530

    UI Update- Improved UI

--- cards.js
+++ cards.js
@@ -1,31 +1,38 @@
 function renderCards(data)
 {
+    if(!data) return;
     var totalCards = data.length;
     var cardtemplate ="";
     for(var i =0;i<totalCards;i++)
     {
         cardtemplate +=  "<div class='column'><div class='card' style='background-color:"+data[i].card.color+"'><span>"+data[i].card.text+"</span></div></div>";
     }
-    document.getElementsByClassName("container")[0].innerHTML=cardtemplate;
-
+    if(document.getElementsByClassName("container").length>0)
+    {
+        document.getElementsByClassName("container")[0].innerHTML=cardtemplate;
+    }
+    cardtemplate = null;
+    totalCards = null;
 }
-
+/** shuffle method */
 function shuffle(data) {
-    let ctr = data.length;
+    if(!data) return;
+    let datalength = data.length;
     let temp;
     let index;
-    while (ctr > 0) {
-        index = Math.floor(Math.random() * ctr);
-        ctr--;
-        temp = data[ctr];
-        data[ctr] = data[index];
+    while (datalength > 0) {
+        index = Math.floor(Math.random() * datalength);
+        datalength--;
+        temp = data[datalength];
+        data[datalength] = data[index];
         data[index] = temp;
     }
     return data;
 }
-
+/** Sort method */
 function sort(data)
 {
+    if(!data) return;
     data.sort(function(a, b){return a.card.value - b.card.value});
     return data;
 }
